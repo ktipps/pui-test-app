a5639e1c8a07a69563a1b9b8784082e2351e7d77
ktipps <62031664+ktipps@users.noreply.github.com>
2021-10-04 11:19:54 -0400

    Running through the rest of the module

--- Lab 6/ main.js
+++ Lab 6/ main.js
@@ -4,7 +4,29 @@ function addNewList() {
 }
 
 function addListItem() {
-    console.log("hello world!")
+    console.log('hello world!');
+    let list = document.getElementById("grocery-list");
+    let itemInput = document.getElementById("new-list-item");
 }
 
+function createElement(){
+    let newItem = document.createElement("li");
+    newItem.appendChild(document.createTextNote(intemInput.value));
+    list.appendChild(newItem);
+}
 
+function deleteListItem(item) {
+    // remove li element (item) from ol element (item.parentNode)
+     item.parentNode.removeChild(item);
+}
+
+function completeListItem(item){
+    if(item.checked){
+        item.parentNode.style.textDecoration = "line-through";
+    }
+    else{
+        item.parentNode.style.textDorartion = "none";
+
+    }
+    }
+}
